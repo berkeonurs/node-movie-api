0ee76afa5658f12cea78be8d07a5a0b5be1bd9aa
Berke Onur Süneçli <onursunecli@hotmail.com>
2019-06-04 23:39:48 +0300

    /movies/between endpoint

--- routes/movie.js
+++ routes/movie.js
@@ -4,6 +4,77 @@ const router = express.Router();
 //Models
 const Movie = require('../models/Movie');
 
+// Tüm Filmleri Çek
+router.get('/', (req, res) => {
+  const promise = Movie.find({  });
+
+  promise.then((data)=> {
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+// Top 10 List
+router.get('/top10', (req, res) => {
+  const promise = Movie.find({  }).limit(10).sort({imdb_score : -1});
+
+  promise.then((data)=> {
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+// Id si verilen filmi çek
+router.get('/:movie_id', (req, res, next) => {
+  const promise = Movie.findById(req.params.movie_id);
+
+  promise.then((movie) => {
+    if (!movie)
+      next({ message: 'The movie was not found.', code:404 });
+
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+// Idsi verilen filmi güncelle
+router.put('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(
+      req.params.movie_id,
+      req.body,
+      {
+        new: true
+      }
+  );
+
+  promise.then((movie) => {
+    if (!movie)
+      next({ message: 'The movie was not found.', code:404 });
+
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+// Idsi verilen filmi sil
+router.delete('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+
+  promise.then((movie) => {
+    if (!movie)
+      next({ message: 'The movie was not found.', code:404 });
+
+    res.json({status: 1});
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+// Film ekle
 router.post('/', (req, res, next) => {
   //const {title, imdb_score, category, country, year} = req.body;
 
@@ -11,10 +82,28 @@ router.post('/', (req, res, next) => {
   const promise = movie.save();
 
   promise.then((data) => {
-    res.json({status: 1});
+    res.json(data);
   }).catch((err) => {
     res.json(err);
   });
 });
+
+//Belirli Tarihler Arası Between
+router.get('/between/:start_year/:end_year', (req, res) => {
+  const { start_year, end_year } = req.params;
+  const promise = Movie.find(
+      {
+        year: { "$gte": parseInt(start_year), "$lte": parseInt(end_year) }
+      }
+  );
+
+  promise.then((data)=> {
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+
 
 module.exports = router;

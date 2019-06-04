68ff07e84d638ee018455c3ab212041d6552af03
Berke Onur Süneçli <onursunecli@hotmail.com>
2019-06-05 00:03:18 +0300

    /api/directors[GET] created

--- routes/director.js
+++ routes/director.js
@@ -16,4 +16,51 @@ router.post('/', (req, res, next) => {
   })
 });
 
+router.get('/',(req, res) => {
+  const promise = Director.aggregate([
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        bio: '$_id.bio',
+        movies: '$movies'
+      }
+    }
+  ]);
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
 module.exports = router;

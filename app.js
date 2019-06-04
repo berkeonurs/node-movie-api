8d1865a62dee217ac720b00ebe83da910b13cb01
Berke Onur Süneçli <onursunecli@hotmail.com>
2019-06-04 23:48:19 +0300

    /api/directors POST Method

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const movie = require('./routes/movie');
+const director = require('./routes/director');
 
 const app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/movies', movie);
+app.use('/api/directors', director);
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
